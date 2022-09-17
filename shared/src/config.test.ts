c46ea28c5d3be61574b0d2d54abc29464d1a3b47
Aktyn <Aktyn3@gmail.com>
2022-09-18 01:11:26 +0200

    Pipeline fix

--- shared/src/config.test.ts
+++ shared/src/config.test.ts
@@ -2,6 +2,6 @@ import { Config } from './config'
 
 describe('Config', () => {
   it('should have correct values and its types', () => {
-    expect(Config.EXAMPLE_VALUE).toBe('example')
+    expect(Config.EXAMPLE_VALUE).toBe('example xyz')
   })
 })
